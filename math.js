b4350fcd66a4284ef0f07b53a3021b8f0e45d045
googlekeep <info.workingforyou@gmail.com>
2022-09-09 21:49:33 +0530

    javascript math

---
+++ math.js
@@ -0,0 +1,13 @@
+// numer ==> 55. 898, -32
+// integer: 2,5,
+// float: decimal : 2.5, 13.67, 32.69
+// operation : AudioDestinationNode, substraction, multiplication, division, remainder
+// PerformanceEntry, 
+// average
+// comparison: >, <, ==, !=, <=
+// oikik: 2 kilo apple and 5 kilo angur:
+// 45, 25, 68, 129, 2 < SVGFEColorMatrixElement, min, average, sum 
+// series:
+// prime 
+// permutation
+
