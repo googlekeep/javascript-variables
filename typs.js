43ad96a2f1f67c9c3ba8432a33e3aab5857ff54b
googlekeep <info.workingforyou@gmail.com>
2022-09-10 23:00:21 +0530

    javascript data types

---
+++ typs.js
@@ -0,0 +1,14 @@
+var price = 100 ;
+// console.log(typeof price);
+var price = '100';
+// console.log (typeof price);
+var is Happy = true ;
+// console.log(typeof isHappy);
+
+var age = 18;                           // number 
+var name = "Jane";                      // string
+var name = {first:"Jane", last:"Doe"};  // object
+var truth = false;                      // boolean
+var sheets = ["HTML","CSS","JS"];       // array
+var a; typeof a;                        // undefined
+var a = null;                           // value null
