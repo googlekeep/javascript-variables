9d18304bbdba3a78e59f89d2aef2e3c17ddd3455
googlekeep <info.workingforyou@gmail.com>
2022-09-10 21:13:20 +0530

    variable

---
+++ variable-name.js
@@ -0,0 +1,24 @@
+// good variable
+
+var price = 23;   
+
+// 1 variable name can not be any keywords
+var false = 96;
+var return = true;
+
+
+// 2 variable name has to be in one work. no space
+var my home address = "new califonia";
+
+
+// 3 variable name can not have quotation
+var "name" = "tom hanks",
+
+// 4 variable name can not starts with a number but can ends with a number
+
+var 99club = 19964;
+vr club25 = 2025;
+
+// 5 how to use long name 
+//  can not use desh
+var not use deshvar user-name = ""
---
+++ variable.js
@@ -0,0 +1,22 @@
+// vary + able = variable
+// javascript variable 5 ta 
+
+// (numbar / numeric )
+var price = 13; (var>name>=man;) (this is variable) 
+var age = 17; (example)
+var age = 2000; (example)
+var temerature = 38;(example)
+
+
+// (String)
+var name = "sodor uddin" ; (variable man jakhon ak ba akadik hobe takhon "" modhe hobe) 
+var location = "andork killa bandor bon "; ( example)
+var special = 'alia bhatt'; (singel '' suru korle singel ' ' diye ses korte hobe)
+
+
+// Boolean
+var serious = true;
+var isFullMarks = true;
+var isSingel = false;
+
+
